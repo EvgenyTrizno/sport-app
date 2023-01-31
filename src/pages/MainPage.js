37a1bf0deb38e6a73e7db3c03da855f856f7b0b0
Женя <zenatrizno9@gmail.com>
2023-01-31 21:50:34 +0300

    paths are set up

---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import PostFeed from "../components/postFeed/PostFeed";
+
+const MainPage = () => {
+    return (
+        <>
+            <PostFeed/>
+        </>
+    );
+};
+
+export default MainPage;
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,11 @@
+import SearchPanel from "../components/searchPanel/SearchPanel";
+
+const SearchPage = () => {
+    return (
+        <>
+            <SearchPanel/>
+        </>
+    );
+};
+
+export default SearchPage;
