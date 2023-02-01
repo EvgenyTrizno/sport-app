740af4f82f0cdd89f966e77869a8f960480d3558
Женя <zenatrizno9@gmail.com>
2023-02-01 22:15:00 +0300

    created a menu for creating posts

---
+++ src/components/shortVideo/ShortVideo.js
@@ -0,0 +1,17 @@
+import './ShortVideo.scss';
+
+const ShortVideo = () => {
+    return (
+        <div className='short-video'>
+            <div className="container">
+                <div className="short-video__inner">
+                    <video src="https://www.youtube.com/watch?v=nUbKDARm_jk">
+                            
+                    </video>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShortVideo;
